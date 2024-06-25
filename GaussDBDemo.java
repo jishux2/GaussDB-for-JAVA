b7eaa80666d1c5d77ccb68f87a6add328ca3bc98
jishux <3418168893@qq.com>
2024-06-25 21:44:50 +0800

    修正查询6

--- GaussDBDemo.java
+++ GaussDBDemo.java
@@ -27,58 +27,74 @@ public class GaussDBDemo {
             int choice; // 用于存储用户的选择
             do {
                 System.out.println("\n欢迎使用GaussDBDemo程序，我们提供以下服务：");
-                System.out.println("1. 用户1点击退出按钮，退出微信，请用SQL语句更新用户的状态。");
-                System.out.println("2. 2021年华为正式发布了基于鸿蒙的手机，鸿蒙操作系统名称是Harmony，请在登录设备中增加一项，设设备的编号是6。");
-                System.out.println("3. 2021年华为正式发布了openEulerc操作系统，请在登录设备中增加一项。");
-                System.out.println("4. 公安局展开新一轮预警，首先需要清除预警表中的所有历史记录，请完成。");
-                System.out.println("5. 预警规则1是单笔转账超过400的用户，将该用户的微信UID以及转账的时间存入Warning表。");
-                System.out.println("6. 预警规则2是累计转出超过1000的用户以及最后一次转账时间存入预警表。");
-                System.out.println("7. 查询");
-                System.out.println("0. 退出程序。");
+                System.out.println("1. 简单增删改");
+                System.out.println("2. 查询");
+                System.out.println("0. 退出程序");
                 System.out.println("请输入你想要执行的功能编号：");
                 choice = sc.nextInt(); // 接收用户输入的整数
                 switch (choice) {
                     case 1:
-                        // 功能1：更新用户1的状态为2（离线）
-                        sql = "UPDATE \"Schema_ljh_zzh\".UserInfo SET Status = 2 WHERE UID = 1";
-                        stmt.executeUpdate(sql); // 执行更新语句
-                        System.out.println("用户1的状态已更新为离线。");
+                        // 功能1：执行简单增删改功能
+                        System.out.println("\n1. 用户1点击退出按钮，退出微信，请用SQL语句更新用户的状态。");
+                        System.out.println("2. 2021年华为正式发布了基于鸿蒙的手机，鸿蒙操作系统名称是Harmony，请在登录设备中增加一项，设设备的编号是6。");
+                        System.out.println("3. 2021年华为正式发布了openEulerc操作系统，请在登录设备中增加一项。");
+                        System.out.println("4. 公安局展开新一轮预警，首先需要清除预警表中的所有历史记录，请完成。");
+                        System.out.println("5. 预警规则1是单笔转账超过400的用户，将该用户的微信UID以及转账的时间存入Warning表。");
+                        System.out.println("6. 预警规则2是累计转出超过1000的用户以及最后一次转账时间存入预警表。");
+                        System.out.println("7. 查询");
+                        System.out.println("0. 返回上一级菜单。");
+                        System.out.println("请输入你想要执行的功能编号：");
+                        int query1 = sc.nextInt(); // 接收用户输入的整数
+                        switch (query1) {
+                            case 1:
+                                // 增删改1：更新用户1的状态为2（离线）
+                                sql = "UPDATE \"Schema_ljh_zzh\".UserInfo SET Status = 2 WHERE UID = 1";
+                                stmt.executeUpdate(sql); // 执行更新语句
+                                System.out.println("用户1的状态已更新为离线。");
+                                break;
+                            case 2:
+                                // 增删改2：在登录设备中增加一项，设备编号为6，名称为Harmony
+                                sql = "INSERT INTO \"Schema_ljh_zzh\".Device VALUES (6, 'Harmony')";
+                                stmt.executeUpdate(sql); // 执行插入语句
+                                System.out.println("登录设备中已增加一项，设备编号为6，名称为Harmony。");
+                                break;
+                            case 3:
+                                // 增删改3：在登录设备中增加一项，设备编号为最大编号加1，名称为openEuler
+                                sql = "INSERT INTO \"Schema_ljh_zzh\".Device(DID, Name) SELECT MAX(DID) + 1, 'openEuler' FROM \"Schema_ljh_zzh\".Device";
+                                stmt.executeUpdate(sql); // 执行插入语句
+                                System.out.println("登录设备中已增加一项，设备编号为最大编号加1，名称为openEuler。");
+                                break;
+                            case 4:
+                                // 增删改4：删除预警表中的所有记录
+                                sql = "DELETE FROM \"Schema_ljh_zzh\".Warning";
+                                stmt.executeUpdate(sql); // 执行删除语句
+                                System.out.println("预警表中的所有记录已删除。");
+                                break;
+                            case 5:
+                                // 增删改5：将单笔转账超过400的用户的微信UID以及转账的时间存入Warning表
+                                sql = "INSERT INTO \"Schema_ljh_zzh\".Warning SELECT UID1, SendTime FROM \"Schema_ljh_zzh\".Transfer WHERE Amount >= 400";
+                                stmt.executeUpdate(sql); // 执行插入语句
+                                System.out.println("单笔转账超过400的用户的微信UID以及转账的时间已存入Warning表。");
+                                break;
+                            case 6:
+                                // 增删改6：将累计转出超过1000的用户以及最后一次转账时间存入预警表
+                                sql = "INSERT INTO \"Schema_ljh_zzh\".Warning SELECT UID1, MAX(SendTime) FROM \"Schema_ljh_zzh\".Transfer GROUP BY UID1 HAVING SUM(Amount) >= 1000";
+                                stmt.executeUpdate(sql); // 执行插入语句
+                                System.out.println("累计转出超过1000的用户以及最后一次转账时间已存入预警表。");
+                                break;
+                            case 0:
+                                // 返回上一级菜单
+                                System.out.println("返回上一级菜单。");
+                                break;
+                            default:
+                                // 输入无效的编号
+                                System.out.println("请输入有效的编号！");
+                                break;
+                        }
                         break;
                     case 2:
-                        // 功能2：在登录设备中增加一项，设备编号为6，名称为Harmony
-                        sql = "INSERT INTO \"Schema_ljh_zzh\".Device VALUES (6, 'Harmony')";
-                        stmt.executeUpdate(sql); // 执行插入语句
-                        System.out.println("登录设备中已增加一项，设备编号为6，名称为Harmony。");
-                        break;
-                    case 3:
-                        // 功能3：在登录设备中增加一项，设备编号为最大编号加1，名称为openEuler
-                        sql = "INSERT INTO \"Schema_ljh_zzh\".Device(DID, Name) SELECT MAX(DID) + 1, 'openEuler' FROM \"Schema_ljh_zzh\".Device";
-                        stmt.executeUpdate(sql); // 执行插入语句
-                        System.out.println("登录设备中已增加一项，设备编号为最大编号加1，名称为openEuler。");
-                        break;
-                    case 4:
-                        // 功能4：删除预警表中的所有记录
-                        sql = "DELETE FROM \"Schema_ljh_zzh\".Warning";
-                        stmt.executeUpdate(sql); // 执行删除语句
-                        System.out.println("预警表中的所有记录已删除。");
-                        break;
-                    case 5:
-                        // 功能5：将单笔转账超过400的用户的微信UID以及转账的时间存入Warning表
-                        sql = "INSERT INTO \"Schema_ljh_zzh\".Warning SELECT UID1, SendTime FROM \"Schema_ljh_zzh\".Transfer WHERE Amount >= 400";
-                        stmt.executeUpdate(sql); // 执行插入语句
-                        System.out.println("单笔转账超过400的用户的微信UID以及转账的时间已存入Warning表。");
-                        break;
-                    case 6:
-                        // 功能6：将累计转出超过1000的用户以及最后一次转账时间存入预警表
-                        sql = "INSERT INTO \"Schema_ljh_zzh\".Warning SELECT UID1, MAX(SendTime) FROM \"Schema_ljh_zzh\".Transfer GROUP BY UID1 HAVING SUM(Amount) >= 1000";
-                        stmt.executeUpdate(sql); // 执行插入语句
-                        System.out.println("累计转出超过1000的用户以及最后一次转账时间已存入预警表。");
-                        break;
-                    // 在switch语句中添加一个case 7，用于执行查询功能
-                    case 7:
-                        // 功能7：执行查询功能
-                        System.out.println("\n请输入你想要执行的查询编号：");
-                        System.out.println("1. 列出每一个用户在每一个设备类型上登录微信次数。");
+                        // 功能2：执行查询功能
+                        System.out.println("\n1. 列出每一个用户在每一个设备类型上登录微信次数。");
                         System.out.println("2. 列出用户1的好友UID。");
                         System.out.println("3. 列出每一个用户的好友数目。");
                         System.out.println("4. 列出用户1和用户3的共同好友。");
@@ -86,8 +102,9 @@ public class GaussDBDemo {
                         System.out.println(
                                 "6. 统计每一个用户对之间的好友数量。");
                         System.out.println("0. 返回上一级菜单。");
-                        int query = sc.nextInt(); // 接收用户输入的整数
-                        switch (query) {
+                        System.out.println("请输入你想要执行的查询编号：");
+                        int query2 = sc.nextInt(); // 接收用户输入的整数
+                        switch (query2) {
                             case 1:
                                 // 查询1：列出每一个用户在每一个设备类型上登录微信次数,按照微信号从小到大输出，相同的微信号，按照登录次数从高到低输出。
                                 sql = "SELECT UID, Name, COUNT(*) AS Total FROM \"Schema_ljh_zzh\".Login, \"Schema_ljh_zzh\".Device WHERE Login.DID = Device.DID GROUP BY UID, Name ORDER BY UID, Total DESC";
@@ -163,13 +180,13 @@ public class GaussDBDemo {
                                 break;
                             case 6:
                                 // 查询6：统计每一个用户对之间的好友数量，如果他们之间没有好友就不显示，每个用户对只显示一次，比如1和2有5个好友，那么显示（1，2，5）或者（2，1，5）。
-                                sql = "SELECT f1.UID1, f2.UID1, COUNT(*) AS Mutual FROM \"Schema_ljh_zzh\".Friend f1, \"Schema_ljh_zzh\".Friend f2 WHERE f1.UID1 < f2.UID1 AND f1.UID2 = f2.UID2 GROUP BY f1.UID1, f2.UID1";
+                                sql = "SELECT f1.UID1 AS UID1_1, f2.UID1 AS UID1_2, COUNT(*) AS Mutual FROM \"Schema_ljh_zzh\".Friend f1, \"Schema_ljh_zzh\".Friend f2 WHERE f1.UID1 < f2.UID1 AND f1.UID2 = f2.UID2 GROUP BY f1.UID1, f2.UID1";
                                 ResultSet rs6 = stmt.executeQuery(sql); // 执行查询语句
                                 System.out.println("每一个用户对之间的好友数量：");
                                 while (rs6.next()) {
                                     // 通过字段检索
-                                    int uid1 = rs6.getInt("f1.UID1");
-                                    int uid2 = rs6.getInt("f2.UID1");
+                                    int uid1 = rs6.getInt("UID1_1");
+                                    int uid2 = rs6.getInt("UID1_2");
                                     int mutual = rs6.getInt("Mutual");
 
                                     // 输出数据
